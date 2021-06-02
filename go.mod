6716e44e397d230af0326acce83bbcc6baa4a9e0
yeejiac <yeejiac@gmail.com>
2021-06-01 22:31:50 -0400

    fix redis connection

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yeejiac/WebAPI_layout
 
 go 1.15
 
-require gopkg.in/ini.v1 v1.62.0
+require (
+	github.com/gomodule/redigo v1.8.4
+	gopkg.in/ini.v1 v1.62.0
+)
--- main.go
+++ main.go
@@ -18,8 +18,26 @@ func handleRequests() {
 }
 
 func main() {
-	log.Println("Start web api")
-	fmt.Println(internal.Test2)
-    internal.SendMail();
-    handleRequests()
+	// log.Println("Start web api")
+	// fmt.Println(internal.Test2)
+    // internal.SendMail();
+    // handleRequests()
+    rc := internal.RedisConnection()
+
+	defer rc.Close()
+
+	key := "KEY"
+	val := "VALUE"
+	internal.RedisSet(key, val, rc)
+	s := internal.RedisGet(key, rc)
+	fmt.Println(s)
+
+	key = "LIST"
+	vallist := []string{"VALUE1", "VALUE2", "VALUE3"}
+	internal.RedisSetList(key, vallist, rc)
+	sl := internal.RedisGetList(key, rc)
+	fmt.Println(sl)
+	fmt.Println(sl[0])
+	fmt.Println(sl[1])
+	fmt.Println(sl[2])
 }
