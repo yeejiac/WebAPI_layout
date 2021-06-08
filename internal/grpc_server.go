6d6cc77ec388f1f6af107f0ae186d7cd858127b7
yeejiac <yeejiac@gmail.com>
2021-06-08 07:41:15 -0400

    add grpc(not set yet)

--- internal/grpc_server.go
+++ internal/grpc_server.go
@@ -1,34 +1,26 @@
 package internal
 
-import (
-	"log"
-	"time"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-)
-
 type EchoServer struct{}
 
-func Start_grpc()
-{
-	apiListener, err := net.Listen("tcp", ":1203")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	else{
-		log.Println(" grpc.Serve bind success: ")
-	}
+// func Start_grpc()
+// {
+// 	apiListener, err := net.Listen("tcp", ":1203")
+// 	if err != nil {
+// 		log.Println(err)
+// 		return
+// 	}
+// 	else{
+// 		log.Println(" grpc.Serve bind success: ")
+// 	}
 
-	grpc := grpc.NewServer()
+// 	grpc := grpc.NewServer()
 
-	reflection.Register(grpc)
-	if err := grpc.Serve(apiListener); err != nil {
-		log.Fatal(" grpc.Serve Error: ", err)
-		return
-	}
-	else{
-		log.Println(" grpc.Serve regist success: ")
-	}
-}
+// 	reflection.Register(grpc)
+// 	if err := grpc.Serve(apiListener); err != nil {
+// 		log.Fatal(" grpc.Serve Error: ", err)
+// 		return
+// 	}
+// 	else{
+// 		log.Println(" grpc.Serve regist success: ")
+// 	}
+// }
