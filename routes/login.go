80302da4cf6466602720d48b03751ffd98473afb
yeejiac <yeejiac@gmail.com>
2021-06-10 05:02:23 -0400

    fix session

--- routes/login.go
+++ routes/login.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -14,7 +15,14 @@ func LoginHandle(w http.ResponseWriter, r *http.Request) {
 		log.Println(t.Execute(w, nil))
 	} else {
 		//請求的是登入資料，那麼執行登入的邏輯判斷
+		usr := strings.Join(r.Form["username"], " ")
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
+		if usr == "123" {
+			fmt.Println("login success")
+			// make session
+		} else {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+		}
 	}
 }
